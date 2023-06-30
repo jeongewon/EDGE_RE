0133d11d8aa82dd729e9a3c37977e2790b52f859
jungsuk chae <portfoliochae@gmail.com>
2023-06-30 21:56:07 +0900

    수정: javascript 내역 수정

--- js/cgv_script.js
+++ js/cgv_script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded');
+
 // HEADER 시작 (유림) //
 // HEADER 끝 (유림) //
 
@@ -10,17 +12,26 @@
 // MAIN_3 시작 (정석) //
 
 
-let gotoTop = document.querySelector('circle_down a');
-let which = window.offsetTop;
 
-gotoTop.addEventListener('click',function(evemt){
+let gotoTop = document.querySelector('.circle_down a');
+let formend = document.querySelectorAll('.footer_form a')
+let scromamt = window.scrollY;
+gotoTop.addEventListener('click', function(e){
 e.preventDefault();
-which.scrollTo(0,0)
-left:0;
-top:0;
-behavior: 'smooth';
-}
-);
+window.scrollTo({
+  top:0,
+  left:0,
+  behavior: 'smooth'
+  });
+});
+
+for(let item of formend){
+  item.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.alert('현재 이동이 불가합니다');
+  });
+});
+
 
 
 // MIIN_3 끝 (정석) //
