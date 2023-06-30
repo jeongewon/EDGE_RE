f0a63fe40f780baa1d2387dd2905847937d5030c
jungsuk chae <portfoliochae@gmail.com>
2023-06-30 21:20:25 +0900

    수정: css, html 및 javascript 추가

--- js/cgv_script.js
+++ js/cgv_script.js
@@ -8,4 +8,19 @@
 // MAIN_2 끝 (수연) //
 
 // MAIN_3 시작 (정석) //
+
+
+let gotoTop = document.querySelector('circle_down a');
+let which = window.offsetTop;
+
+gotoTop.addEventListener('click',function(evemt){
+e.preventDefault();
+which.scrollTo(0,0)
+left:0;
+top:0;
+behavior: 'smooth';
+}
+);
+
+
 // MIIN_3 끝 (정석) //
