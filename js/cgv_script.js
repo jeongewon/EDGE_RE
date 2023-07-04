2c0b8c4d873f1bb109476be19f7e41ce16c1f8a1
jeongewon <2wonyee@gmail.com>
2023-07-04 18:22:14 +0900

    230704 수정4

--- js/cgv_script.js
+++ js/cgv_script.js
@@ -83,52 +83,25 @@ modalClose.addEventListener('click',()=>{
 /* MAIN_1 시작 (이원) */
 // 메인 영상 버튼
 let video = document.querySelector('#Elemental'),
-PlayBtn = document.querySelector('.playbtn'),
-PauseBtn = document.querySelector('.pausebtn'),
-SoundBtn = document.querySelectorAll('.sound button'),
-videoSoundBtn = document.querySelector('.soundbtn'),
+// PlayBtn = document.querySelector('.playbtn'),
+// PauseBtn = document.querySelector('.pausebtn'),
+// SoundBtn = document.querySelectorAll('.sound button'),
+// videoSoundBtn = document.querySelector('.soundbtn'),
 videoSoundoffBtn = document.querySelector('.soundoffbtn'),
-videoToggleBtn = document.querySelectorAll('.mVbtn');
+videoToggleBtn = document.querySelector('.mVbtn'),
+videoSoundBtn = document.querySelector('.sound');
 
-
-
-videoSoundoffBtn.addEventListener('click', ()=>{
-  video.muted = false;
-  videoSoundoffBtn.style.display = 'none';
-  videoSoundBtn.style.display = 'block';
-}); 
-
-videoSoundBtn.addEventListener('click', ()=>{
-  video.muted = true;
-  videoSoundBtn.style.display = 'none';
-  videoSoundoffBtn.style.display = 'block';
-}); 
-
-PlayBtn.addEventListener('click',()=>{
-  PlayBtn.classList.remove('active');
-  video.play();
-  PlayBtn.style.display = 'none';
-  PauseBtn.style.display = 'block';
-})
-
-PauseBtn.addEventListener('click',()=>{
-  PauseBtn.classList.remove('active');
-  video.pause();
-  PauseBtn.style.display = 'none';
-  PlayBtn.style.display = 'block';
-});
-
-
-// if(videoSoundoffBtn.classList.add('active')){
+// videoSoundoffBtn.addEventListener('click', ()=>{
 //   video.muted = false;
 //   videoSoundoffBtn.style.display = 'none';
 //   videoSoundBtn.style.display = 'block';
-// } else {
-//   video.muted = true;
-//   videoSoundoffBtn.style.display = 'block';
-//   videoSoundBtn.style.display = 'none';
-// }
+// }); 
 
+// videoSoundBtn.addEventListener('click', ()=>{
+//   video.muted = true;
+//   videoSoundBtn.style.display = 'none';
+//   videoSoundoffBtn.style.display = 'block';
+// }); 
 
 //영상 슬라이드 구현
 let videoslideWrapper = document.querySelector('.MovieVideo_slide_wrap'), //ul
@@ -136,14 +109,109 @@ let videoslideWrapper = document.querySelector('.MovieVideo_slide_wrap'), //ul
     videoslideCount = videoslides.length,
     videoslideWidth = 1272,
     videoslideMargin = 30,
+    showSlides = 3,
     videocurrentIdx = 0,
     videoprevCrousel = document.querySelector('.mV_leftCrousel'),
     videonextCrousel = document.querySelector('.mV_rightCrousel');
 
+//복사본 생성하기
+    for(var i = 0;i<videoslideCount;i++){
+      var cloneSlide = videoslides[i].cloneNode(true);
+      cloneSlide.classList.add('clone');
+      videoslideWrapper.appendChild(cloneSlide);
+  }
+  for(var i = videoslideCount -1; i>=0 ; i--){
+      var cloneSlide = videoslides[i].cloneNode(true);
+      cloneSlide.classList.add('clone');
+      videoslideWrapper.prepend(cloneSlide);
+  }
+
+//슬라이드 배치
+let allSlides = videoslideWrapper.querySelectorAll('li');
+videoslideWrapper.style.width = videoslideWidth * allSlides.length + 'px';
+
+//슬라이드 가운데로 재배치하기
+function setSlide(){
+  videoslideWrapper.style.transform = `translateX(-${videoslideWidth*(showSlides+1)}px)`;
+  setTimeout(()=>{videoslideWrapper.classList.add('animated')},100);
+}
+setSlide();
+
+//SLIDE이동함수
+function moveslide(num){
+  videoslideWrapper.style.left = -num * videoslideWidth + 'px';
+  videocurrentIdx = num;
+  console.log(videocurrentIdx);
+  let activeIdx = videocurrentIdx + showSlides+1;
+  for(slide of allSlides){
+    slide.classList.remove('active');
+  }
+    allSlides[activeIdx].classList.add('active');
+
+  allSlides.forEach(item=>{
+    let slideVideo = item.querySelector('video');
+    slideVideo.pause();
+  });
+  
 
 
 
-// 타이틀 클릭
+  if(videocurrentIdx == showSlides || videocurrentIdx == -showSlides){
+      setTimeout(function(){
+        videoslideWrapper.classList.remove('animated');
+        videoslideWrapper.style.left = '0px'; 
+        videocurrentIdx = 0;
+
+        for(slide of allSlides){
+          slide.classList.remove('active');
+          console.log('active제거');
+        }
+        allSlides[4].classList.add('active');
+      },500);
+
+      setTimeout(function(){
+        videoslideWrapper.classList.add('animated');
+      },600);
+  }
+  videoToggleBtn.classList.remove('active');
+  videoSoundBtn.classList.remove('active');
+}
+moveslide(0);
+
+//버튼으로 이동하기
+ videonextCrousel.addEventListener('click',()=>{
+  moveslide(videocurrentIdx+1);
+})
+videoprevCrousel.addEventListener('click',()=>{
+  moveslide(videocurrentIdx-1);
+})
+
+// 영상 재생 버튼 및 음소거 버튼
+// let activeVideo = videoslideWrapper.querySelector('.active video');
+
+
+
+videoToggleBtn.addEventListener('click',()=>{
+  videoToggleBtn.classList.toggle('active');
+  let activeVideo = videoslideWrapper.querySelector('.active video');
+  if(videoToggleBtn.classList.contains('active')){
+    activeVideo.play();
+  }else{
+    activeVideo.pause();
+  }
+})
+
+videoSoundBtn.addEventListener('click',()=>{
+  videoSoundBtn.classList.toggle('active');
+  let activeVideo = videoslideWrapper.querySelector('.active video');
+  if(videoSoundBtn.classList.contains('active')){
+    activeVideo.muted = true;
+  } else {
+    activeVideo.muted = false;
+  }
+})
+
+// 무비차트 & 상영예정작 타이틀 클릭
 let movieChart = document.querySelector('#btnMovieChart'),
 movieReserve = document.querySelector('#btnReserMovie'),
 allTitle = document.querySelectorAll('.movieChart_tt a'),
@@ -164,7 +232,6 @@ allTitle.forEach(item=>{
     document.querySelector(target).style.display = 'block';
   })
 })
-
 
 // 무비차트 슬라이드 구현
 let slideContainers = document.querySelectorAll('.slidewrapper')
@@ -212,51 +279,6 @@ function multipleSlide(target){
       }
     });
 }
-
-
-
-// function multipleSlide(target){
-  
-//   let slideContainer = document.querySelector('.slidewrapper .MovieChart_wrap'),
-//   slideWrapper = document.querySelector('.slidewrapper .MovieChart_slide_wrap'),
-//   slides = document.querySelectorAll('.slidewrapper .MovieChart_slide_wrap li'),
-//   slideCount = slides.length,
-//   slideWidth = 222,
-//   slideMargin = 40,
-//   slidesPerView = 5,
-//   currentIdx = 0,
-//   prevBtn = document.querySelector('#prev'),
-//   nextBtn = document.querySelector('#next');
-  
-//   slideWrapper.style.width = slideCount*(slideWidth+slideMargin)+'px';
-  
-//   function moveSlides(num){
-//       slideWrapper.style.left = -num*(slideWidth+slideMargin)+'px';
-//       currentIdx = num;
-  
-//       if(currentIdx === 0){
-//           prevBtn.style.visibility = 'hidden';
-//           nextBtn.style.visibility = 'visible';
-//       } else {
-//          prevBtn.style.visibility = 'visible';
-//          nextBtn.style.visibility = 'hidden';
-//       }
-//   }
-//   moveSlides(0)
-  
-//   nextBtn.addEventListener('click',()=>{
-//       if(currentIdx < slideCount-slidesPerView){
-//         moveSlides(currentIdx+5);
-//       }
-//     });
-//   prevBtn.addEventListener('click',()=>{
-//       if(currentIdx > 0){
-//         moveSlides(currentIdx-5);
-//       }
-//     });
-
-// }
-
 /* MAIN_1 끝 (이원) */
 
 /* MAIN_2 시작 (수연) */
