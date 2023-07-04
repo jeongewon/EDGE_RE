ef8d6315b99a193217c37d3258b07da0fd2c2f7a
rhslfmafma <portfoliochae@gmail.com>
2023-07-05 08:56:15 +0900

    merge:main into 정석

--- js/cgv_script.js
+++ js/cgv_script.js
@@ -1,24 +1,3 @@
-
-
-document.addEventListener('DOMContentLoaded',()=>{
-
-
-// HEADER 시작 (유림) //
-// HEADER 끝 (유림) //
-
-// MAIN_1 시작 (이원) //
-// MAIN_1 끝 (이원) //
-
-// MAIN_2 시작 (수연) //
-// MAIN_2 끝 (수연) //
-
-// MAIN_3 시작 (정석) //
-
-
-
-let gotoTop = document.querySelector('.circle_down a');
-let formend = document.querySelectorAll('.footer_form a')
-
 /* HEADER 시작 (유림) */
 
 /* POP UP PORTFOLIO NOTICE */
@@ -498,9 +477,6 @@ enextBtn.addEventListener('click', e_debounce(()=>{
 let gotoTop = document.querySelector('.circle_down');
 let formend = document.querySelectorAll('.footer_form a')
 // let btntouch = document.querySelector('.circle_pop a');
-
-
-// let btntouch = document.querySelector('.circle_pop a');
 let scromamt = window.scrollY;
 gotoTop.addEventListener('click', function(e){
 e.preventDefault();
@@ -517,6 +493,4 @@ for(let item of formend){
     window.alert('현재 이동이 불가합니다');
   });
 };
-});
-
 // MIIN_3 끝 (정석) //
